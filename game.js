3063c64b4a55f3be42801aacf041835b46be81df
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-05-12 15:43:11 +0900

    Update game.js

--- game.js
+++ game.js
@@ -82,9 +82,9 @@ class PreloadScene extends Phaser.Scene {
         });
 
         // 画像の読み込み
-        this.load.image(CONFIG.IMG_JAKO, 'assets/jako.png');
-        this.load.image(CONFIG.IMG_TAKO, 'assets/tako.png');
-        this.load.image(CONFIG.IMG_KAI, 'assets/kai.png');
+        this.load.image(CONFIG.IMG_JAKO, 'jako.png');
+        this.load.image(CONFIG.IMG_TAKO, 'tako.png');
+        this.load.image(CONFIG.IMG_KAI, 'kai.png');
         // 必要なら背景画像などもここで
         // this.load.image('background', 'img/background.png');
     }
