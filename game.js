bd2e7ff4ffe41c0becbc188889b578791cd03a53
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-05-12 19:41:34 +0900

    テキスト

--- game.js
+++ game.js
@@ -35,7 +35,7 @@ const CONFIG = {
         { id: 'tako_ink', name: 'スミはきだこ', imageKey: 'collect_tako_6', description: '驚くとすぐに墨を吐いて逃げる。' },
 
  { id: 'tako_2', name: 'トリアエズコレ', imageKey: 'collect_tako_4', description: 'サプ不可文化は\n無くした方がいい' },
-        { id: 'tako_ink3', name: 'スミだこ', imageKey: 'collect_tako_5', description: '驚くとすぐに墨を吐いて逃げる。' },
+        { id: 'tako_ink3', name: 'ミュルグレス', imageKey: 'collect_tako_5', description: '貴重な水三手' },
         { id: 'tako_ink4', name: 'はきだこ', imageKey: 'collect_tako_7', description: '驚くとすぐに墨を吐いて逃げる。' },
         { id: 'tako_ink5', name: 'スミルミナス', imageKey: 'collect_tako_8', description: '驚くとすぐに墨を吐いて逃げる。' },
         { id: 'tako_ink6', name: 'クオリティオブライフ', imageKey: 'collect_tako_9', description: '水であり酸素' },
